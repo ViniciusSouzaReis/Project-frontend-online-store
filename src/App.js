d1ce2f74834ff172c3e569a8353980305f50f268
Vinicius Luiz de Souza Reis <vinniiesouza@gmail.com>
2022-08-05 17:42:34 -0300

    Adiciona link para o carrinho de compras e implementa a logica no componente ShoppingCartList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Search from './components/Search';
+import ShoppingCartList from './components/ShoppingCartList';
 
 export default class App extends Component {
+  state = {
+    shoppingCartContent: '',
+  }
+
   render() {
+    const { shoppingCartContent } = this.state;
     return (
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Search } />
+          <Route
+            exact
+            path="/shoppingcart"
+            render={
+              () => (<ShoppingCartList shoppingList={ shoppingCartContent } />)
+            }
+          />
         </Switch>
       </BrowserRouter>
     );
