f05a59e53bd692133ccb28b384013c450ee52f4a
rafaelftourinho <rafaelftourinho@gmail.com>
2022-08-05 18:59:24 -0300

    :bug: Removendo erro de lint

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { getProductsFromCategoryAndQuery } from '../services/api';
 import { Link } from 'react-router-dom';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 export default class Search extends Component {
   state = {
