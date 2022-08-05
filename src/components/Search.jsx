a5fb8f41136cf8d8d30f7310ec523d5050d1f8fd
Antonio Araujo <cnysini@hotmail.com>
2022-08-05 18:04:07 -0300

    Create a button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,14 +7,25 @@ export default class Search extends Component {
 
   onInputChange = ({ target }) => {
     const { name, value } = target;
-    this.setState({ [name]: value });
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleChangeButton = () => {
+    console.log('oi');
   }
 
   render() {
     const { searchValue } = this.state;
     return (
       <div>
-        <input type="text" name="searchValue" onChange={ this.onInputChange } />
+        <input
+          type="text"
+          name="searchValue"
+          onChange={ this.onInputChange }
+          data-testid="query-input"
+        />
         {!searchValue
           && (
             <span
@@ -22,6 +33,14 @@ export default class Search extends Component {
             >
               Digite algum termo de pesquisa ou escolha uma categoria.
             </span>)}
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={ this.handleChangeButton }
+        >
+          Pesquisar
+
+        </button>
       </div>
     );
   }
