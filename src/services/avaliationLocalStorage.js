a51702ed901d4475205fc3f48415b9da09d4349f
vinicius <vinnyzsouza@gmail.com>
2022-08-08 19:20:22 -0300

    req11 corrigido

--- src/services/avaliationLocalStorage.js
+++ src/services/avaliationLocalStorage.js
@@ -3,16 +3,16 @@ const PRODUCT_KEY = 'avaliationList';
 if (!JSON.parse(localStorage.getItem(PRODUCT_KEY))) {
   localStorage.setItem(PRODUCT_KEY, JSON.stringify([]));
 }
-const readProductCard = () => JSON.parse(localStorage.getItem(PRODUCT_KEY));
+const readProductCard = (id) => JSON.parse(localStorage.getItem(id)) || [];
 
-const saveProductCard = (product) => localStorage
-  .setItem(PRODUCT_KEY, JSON.stringify(product));
+const saveProductCard = (id, product) => localStorage
+  .setItem(id, JSON.stringify(product));
 
-export const getListAvaliation = () => readProductCard();
+export const getListAvaliation = (id) => readProductCard(id);
 
-export const addAvaliation = (product) => {
+export const addAvaliation = (id, product) => {
   if (product) {
-    const favoriteProducts = readProductCard();
-    saveProductCard([...favoriteProducts, product]);
+    const favoriteProducts = readProductCard(id);
+    saveProductCard(id, [...favoriteProducts, product]);
   }
 };
